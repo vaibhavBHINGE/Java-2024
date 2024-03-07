03f121a87bce0120e140e095184901450c5d317c
vaibhav bhinge <vbhinge2711@gmail.com>
2024-03-07 23:27:41 +0530

    Variable

---
+++ src/Java/Variable.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		String str="Vaibhav";
+		System.out.println("Variable is used to contain data or we can allocate memory and our variable name is 'str' data in variable is "+str);
+	}
+
+}
