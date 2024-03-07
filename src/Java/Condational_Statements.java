a0241885b4349a72045ce1ed14d795b97aca3ce2
vaibhav bhinge <vbhinge2711@gmail.com>
2024-03-07 23:47:07 +0530

    Create Condational_Statements.java
    If Else control statement to check users vote eligibility

---
+++ src/Java/Condational_Statements.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Condational_Statements {
+
+	public static void main(String []args) {
+		 //Check whether person is eligible for vote or not
+		int person=23;
+		if(person>=18)
+		{
+			System.out.println("Person is eligible for vote");
+		}
+		else
+		{
+			System.out.println("person is not eligible for vote");
+		}
+	}
+}
