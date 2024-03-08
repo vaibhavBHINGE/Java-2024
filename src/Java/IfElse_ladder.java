0bc83ff3a45423eeda335a5a539def39e96723c9
vaibhav bhinge <vbhinge2711@gmail.com>
2024-03-08 23:54:20 +0530

    Create IfElse_ladder.java
    Here is an example of an if-else ladder that displays numbers as words between 1 and 10.

---
+++ src/Java/IfElse_ladder.java
@@ -0,0 +1,69 @@
+package Java;
+
+import java.util.Scanner;
+
+public class IfElse_ladder {
+
+	public static void main(String[] args) {
+		
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter numner between 1-10 to convert into word:");
+				int n=sc.nextInt();
+				
+				if(n==1)
+				{
+				System.out.println("Given number is One");	
+				}
+				else if(n==2)
+				{
+					System.out.println("Given number is Two");	
+
+				}
+				else if(n==3)
+				{
+					System.out.println("Given number is Three");	
+
+				}
+				else if(n==4)
+				{
+					System.out.println("Given number is Four");	
+
+				}
+				else if(n==5)
+				{
+					System.out.println("Given number is Five");	
+
+				}
+				else if(n==6)
+				{
+					System.out.println("Given number is Six");	
+
+				}
+				else if(n==7)
+				{
+					System.out.println("Given number is Seven");	
+
+				}
+				else if(n==8)
+				{
+					System.out.println("Given number is Eight");	
+
+				}
+				else if(n==9)
+				{
+					System.out.println("Given number is Nine");	
+
+				}
+				else if(n==10)
+				{
+					System.out.println("Given number is Ten");	
+
+				}
+				else {
+					System.out.println("Given number is above Ten so pleas enumber inbetween 1-10");
+				}
+
+			}
+
+		}
+
