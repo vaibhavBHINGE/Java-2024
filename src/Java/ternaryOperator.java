9624f53c9bf669974969a8dfb31eb7d377d7a850
vaibhav bhinge <vbhinge2711@gmail.com>
2024-03-11 16:50:46 +0530

    Update ternaryOperator.java

--- src/Java/ternaryOperator.java
+++ src/Java/ternaryOperator.java
@@ -1,10 +1,18 @@
 package Java;
 
+import java.util.Scanner;
+
 public class ternaryOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int max,a,b;
+		System.out.println("Enter first number: ");
+		a=sc.nextInt();
+		System.out.println("Enter Second Number");
+		b=sc.nextInt();
+		max=(a>b)?a:b;
+		System.out.println(max+" is the grater numer");	
 	}
 
 }
